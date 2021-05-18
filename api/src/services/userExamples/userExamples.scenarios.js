2f639db7f72364cffdc0e00c4ddbd32729a4eccb
Andrew <andrew_lam@hwahin.com.my>
2021-05-18 19:01:52 +0800

    yarn rw generate scaffold UserExample

---
+++ api/src/services/userExamples/userExamples.scenarios.js
@@ -0,0 +1,6 @@
+export const standard = defineScenario({
+  userExample: {
+    one: { email: 'String9544602' },
+    two: { email: 'String3668961' },
+  },
+})
--- web/src/Routes.js
+++ web/src/Routes.js
@@ -12,6 +12,10 @@ import { Router, Route } from '@redwoodjs/router'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/user-examples/new" page={NewUserExamplePage} name="newUserExample" />
+      <Route path="/user-examples/{id:Int}/edit" page={EditUserExamplePage} name="editUserExample" />
+      <Route path="/user-examples/{id:Int}" page={UserExamplePage} name="userExample" />
+      <Route path="/user-examples" page={UserExamplesPage} name="userExamples" />
       <Route notfound page={NotFoundPage} />
     </Router>
   )
